54ec3a0e4d1327cbabd6c74552886b877f9e6a48
mayank281 <mayankrajpu3012@gmail.com>
2022-02-17 19:03:43 +0530

    initial commit

---
+++ lyricsgenrator.py
@@ -0,0 +1,35 @@
+from tkinter import *
+from lyrics_extractor import SongLyrics
+ 
+
+def get_lyrics():
+    extract_lyrics = SongLyrics("AIzaSyAYesmcC66_NvWhDbbFurx1IP0ovNUintA","3a5e3982a89326de3") 
+    temp=extract_lyrics.get_lyrics(str(e.get()))
+    res=temp['lyrics']
+    result.set(res)
+    
+    
+master=Tk()
+master.geometry("650x750")
+master.title("Lyrics Extractor")
+master.configure(bg='light blue')
+result=StringVar()
+Label(master, text="Lyrics Generator",bg="light blue", font="comicsansms 18 bold", justify=CENTER, pady=15, padx=20).grid(row=0, column=1)
+Label(master, text="Enter Song name : ",
+      bg="cyan",font="comicsansms 13 bold",width=20).grid(row=1, sticky=W)
+
+Label(master, text="Result :",
+      bg="cyan",font="comicsansms 13 bold",width=20).grid(row=4, sticky=W)
+Label(master, text="", textvariable=result,
+      bg="light blue").grid(row=5, column=1)
+e = Entry(master, width=50)
+e.grid(row=1, column=1)
+ 
+# creating a button using the widget
+b = Button(master, text="Show",
+           command=get_lyrics, bg="White")
+ 
+b.grid(row=1, column=2, columnspan=2,
+       rowspan=2, padx=5, pady=5,)
+ 
+mainloop()
